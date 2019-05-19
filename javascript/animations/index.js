2961cf0e01820897bf28e3383a82ba41c8097385
Adela Spirkova <ad.spirkova@gmail.com>
2019-05-19 12:55:45 +0200

    Weekly update

---
+++ javascript/animations/index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener('click', () => {
+        const box = document.querySelector('div');
+        box.classList.add('switch');
+    })
+});
---
+++ javascript/login/index2.js
@@ -0,0 +1,24 @@
+let username = ['adela', 'adam', 'pavel', 'monika'];
+/* 
+const loop = () => {for (i = 0; i < username.length; i++){
+    console.log(String(username[i]));
+};} */
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const Click = document.querySelector('#submit');
+    Click.addEventListener('click', loop)
+});
+
+
+const loop = () =>{
+        console.log('hello');
+        let x = 0;
+        for ( i = 0; i <= username.length; i++){
+        x=i;
+    }
+    console.log(x);
+    
+        
+        
+};
