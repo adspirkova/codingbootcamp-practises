1acf1be9798ef4285b7a4d80e80f639567a988f4
Adela Spirkova <ad.spirkova@gmail.com>
2019-05-31 10:41:50 +0200

    adding workout

--- BE/php/php-intro/exercise/arrays/chessboard playground/chess.php
+++ BE/php/php-intro/exercise/arrays/chessboard playground/chess.php
@@ -10,7 +10,9 @@ $black = 'black';
     <?php for ($i = 0; $i < 8; $i++) :?>
     <div class="row">
         <?php for ($j = 0; $j < 8; $j++) :?>
-        <div class=<?= ($i + $j ) %2 ? $black : $white ?>></div>
+        <div class=<?= ($i + $j ) %2 ? $black : $white ?>>
+        <?= $i == 3 && $j == 1  ? '<div> <img  src="http://classes.codingbootcamp.cz/assets/classes/33/pieces/whites/king.png" alt="king"> </div>' : "" ?>
+        </div>
         <?php endfor; ?> 
     </div>
    
